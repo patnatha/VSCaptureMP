be15dd4727e595790a807a92c158f1e583d661b9
Nathan Patel <nthnpatel@gmail.com>
2022-04-09 10:20:14 -0400

    made the logging dynamic based on scanning the local LAN

--- run_logger.py
+++ run_logger.py
@@ -4,31 +4,33 @@ import subprocess
 import signal
 import sys
 
-intellivue_lan = "192.168.8.217"
-intellivue_default_port = 443
+intellivue_lan = None
+intellivue_default_port = 24105
 def scan_lan():
     devices = []
     for device in os.popen('arp -a'): devices.append(device)
-    
+    print("Scanning LAN, found " + str(len(devices)) + " devices")
+
     for dev in devices:
         parseLine = dev.split(" ")
         if(len(parseLine) > 2):
             ipaddr = parseLine[1].strip(")").strip("(")
-
-            for line in os.popen('nc -n -v -G 3 ' + ipaddr + ' ' + 
+            
+            for line in os.popen('nc -v -u -w 3 ' + ipaddr + ' ' + 
                                  str(intellivue_default_port) + ' 2>&1'):
-                if("failed" in line):
+                if("failed" in line or "timed out" in line):
                     print(ipaddr, "Failed")
+                    continue
                 elif("succeeded" in line):
                     print(ipaddr, "Success")
-                #else:
-                #    print(line)
-scan_lan()
-sys.exit(1)
+                    return ipaddr
+                else:
+                    print(ipaddr, "UNKNOWN RESP", line)
 
 #these are constants to run the program
 command_path = "/home/pi/Documents/VSCaptureMP/bin/Debug/net6.0"
-command = "dotnet VSCaptureMP.dll -mode 1 -port " + intellivue_lan +" -interval 3 -waveset 7 -scale 2 -export 2"
+commandPre = "dotnet VSCaptureMP.dll -mode 1 -port " 
+commandPost = " -interval 3 -waveset 7 -scale 2 -export 2"
 
 def is_logger_alive():
     result = subprocess.run(['ps','-aux'], stdout=subprocess.PIPE)
@@ -51,19 +53,22 @@ signal.signal(signal.SIGTERM, sighandler)
 
 def start_logger():
     print("\tStarting Logger")
-    os.system("cd " + command_path + "; " + command + "&")
+    os.system("cd " + command_path + "; " + commandPre + 
+              intellivue_lan + commandPost + "&")
 
 while True:
-    #Check to see if monitor is on
-    response = subprocess.run(["ping", "-c", "1", "-w2", intellivue_lan], 
-                stdout=subprocess.DEVNULL).returncode
-    if(response == 0):
+    #Check to see if monitor is up on the LAN
+    intellivue_lan = scan_lan()
+
+    #If monitor is up then start logger
+    if(intellivue_lan != None):
         print("Host is up")
         if(not is_logger_alive()):
             start_logger()
         else:
             print("\tLogger is Alive")
-    elif(response == 1):
+    #If monitor is down then stop logger
+    elif(intellivue_lan == None):
         print("Host is down")
         if(is_logger_alive()):
             stop_logger()
